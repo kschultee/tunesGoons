26a311829320bc61ada18034df30b244b49c6dcc
Kevin Schulte <kschultee@gmail.com>
2018-05-10 23:31:02 -0700

    Added Profile component #19

---
+++ client/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: ''
+    }
+  }
+}
+
+export default Profile
